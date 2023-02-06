c7d32ae4d87e8893b8f5d94dd762743f5e2c0ffb
Bhushan <bhushan2005@gmail.com>
2023-02-07 00:16:13 +0530

    initial version

--- README.md
+++ README.md
@@ -17,14 +17,7 @@ RxJava3 From Udemy
 
 ## FAQ
 
-1. How to setup Hello World program?  
- - Add rxjava maven dependency from maven repo.
- - Create Observable (Subject) which generates events for string Hello and World.
- - Create observers (subscribers) to the subject and write the callbacks (which print the event)
- - The program when run gives followng output
-
-> Observer 1 Hello  
-> Observer 1 World  
-> Observer 2 Hello  
-> Observer 2 World
+1. How to rxjava Observable and Observer?  
+ - Here, the methods onNext(), onComplete() and onError() are part of Emitter interface.  
+ - These methods are also part of Observer interface.
 
--- src/main/java/bt/rx/udrxjava3/HelloWorld.java
+++ src/main/java/bt/rx/udrxjava3/HelloWorld.java
@@ -1,17 +1,58 @@
 package bt.rx.udrxjava3;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.internal.operators.observable.ObservableCreate;
+
+
 public class HelloWorld {
 
     public static void main (String[] args) {
 
-        Observable<String> source = Observable.create(
-                e -> {
-                    e.onNext("Hello");
-                    e.onNext("World");
-                });
-        source.subscribe(e -> System.out.println("Observer 1 "+e));
-        source.subscribe(e -> System.out.println("Observer 2 "+e));
+        Observable<Integer> source = new ObservableCreate<>(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Throwable {
+                try {
+                    emitter.onNext(10);
+                    emitter.onNext(10);
+                    emitter.onComplete();
+                    // The below onNext is not called as onComplete is already called.
+                    emitter.onNext(20);
+                } catch(Throwable t) {
+                    emitter.onError(t);
+                }
+            }
+        });
+
+        Observer<Integer> observer = new Observer<>() {
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                System.out.println("Subscribed");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer t) {
+                System.out.println("On Next : "+t);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Completed !");
+            }
+        };
+
+        source.subscribe(observer);
     }
 
 }
